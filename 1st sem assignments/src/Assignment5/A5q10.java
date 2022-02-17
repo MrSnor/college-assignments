935885b1884bdb2daa60a20bf44f4cfbe318b80e
MrSnor <parthap.das36@gmail.com>
2022-02-17 14:56:27 +0530

    updating some problems

--- 1st sem assignments/src/Assignment5/A5q10.java
+++ 1st sem assignments/src/Assignment5/A5q10.java
@@ -1,35 +1,43 @@
 package Assignment5;
 
+// loooks correct most likely
 public class A5q10 {
     public static void main(String[] args) {
-        sinFormula(1.57);
+        sinFormula(45);
     }
 
     public static void sinFormula(double x) {
+        x = Math.toRadians(x);
+        // variable to be used for the infinite sum
         double sum = x;
-        double error = Math.pow(10, -3);
+        double error = Math.pow(10, -6);
+        // term for EACH sum
         double term = 0;
         int ncount = 1;
+        int counter = 1;
         while (true) {
-            if (ncount % 2 == 1) {
-                term = Math.pow(x, ncount + 2) / factorial2(ncount + 2);
+            if (counter % 2 == 1) {
+                term = -(Math.pow(x, ncount + 2) / factorial(ncount + 2));
             } else {
-                term = -(Math.pow(x, ncount + 2) / factorial2(ncount + 2));
+                term = Math.pow(x, ncount + 2) / factorial(ncount + 2);
             }
-            if (term < error) {
+            if (Math.abs(term) < error) {
                 break;
             }
             sum += term;
-            ncount++;
+            ncount += 2;
+            counter++;
         }
-        System.out.println(sum);
+        // showing the value to 2 places after decimal
+        System.out.println(String.format("%.2f",sum)); // string formatting
     }
 
-    static double factorial2(double i) {
-        double product = 1;
-        if (i == 0)
-            return 1;
-        return i * factorial2(i - 1);
+    static double factorial(double i) {
+        int product = 1;
+        for (double j = i; j > 0; j--) {
+            product*=j;
+        }
+        return product;
     }
 
 }
--- 1st sem assignments/src/classWork/q16.java
+++ 1st sem assignments/src/classWork/q16.java
@@ -2,16 +2,17 @@ package classWork;
 // assignment problem A5q8
 public class q16 {
     public static void main(String[] args) {
-        System.out.println(factorial2(5));
+        System.out.println(factorial1(3));
+        System.out.println(factorial2(3));
     }
 
-    // public static int factorial1(int i) {
-    //     int product = 1;
-
-    //     for (int j = 0; j < 0; j++) {
-            
-    //     }
-    // }
+    public static int factorial1(int i) {
+        int product = 1;
+        for (int j = i; j > 0; j--) {
+            product*=j;
+        }
+        return product;
+    }
 
     static int factorial2(int i) {
         if (i == 0)
